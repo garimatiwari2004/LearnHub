7f58adb1c1d57258a45bdd75b4b62b2550d9520a
Garima Tiwari <garimatiwari1803@gmail.com>
2025-05-25 23:25:22 +0530

    built homepage

---
+++ src/component/home/success.jsx
@@ -0,0 +1,62 @@
+import './success.css';
+export default function Success() {
+  return (
+    <div className="success-wrapper">
+      <div className="success-container">
+       
+        <div className='success-text'>
+          <h2>Everything you need to succeed!</h2>
+          <h6>Our platform is designed with features that make</h6>
+          <h6>learning effective, engaging, and accessible for everyone.</h6>
+        </div>
+        <div className='quality'>
+            <div className='quality-item'>
+                <i class="fa-solid fa-book-open"></i>
+                <h3>Extensive Course Library</h3>
+                <p>Access thousands of courses across various</p>
+                <p>disciplines, from programming to design, business,</p>
+                <p>and more.</p>
+            </div>
+            <div className='quality-item'>
+                <i class="fa-solid fa-users"></i>
+                <h3>Expert Instructors</h3>
+                <p>Access thousands of courses across various</p>
+                <p>disciplines, from programming to design, business,</p>
+                <p>and more.</p>
+            </div>
+            <div className='quality-item'>
+                <i class="fa-solid fa-user-plus"></i>
+                <h3>Community Support</h3>
+                <p>Access thousands of courses across various</p>
+                <p>disciplines, from programming to design, business,</p>
+                <p>and more.</p>
+            </div>
+            
+        </div>
+        <div className='quality'>
+            <div className='quality-item'>
+                <i class="fa-solid fa-diamond"></i>
+                <h3>Personalized Experience</h3>
+                <p>Access thousands of courses across various</p>
+                <p>disciplines, from programming to design, business,</p>
+                <p>and more.</p>
+            </div>
+            <div className='quality-item'>
+                <i class="fa-solid fa-laptop-file"></i>
+                <h3>Flexible Learning</h3>
+                <p>Access thousands of courses across various</p>
+                <p>disciplines, from programming to design, business,</p>
+                <p>and more.</p>
+            </div>
+            <div className='quality-item'>
+                <i className="fa-solid fa-check"></i>
+                <h3>Interactive Learning</h3>
+                <p>Access thousands of courses across various</p>
+                <p>disciplines, from programming to design, business,</p>
+                <p>and more.</p>
+            </div>
+      </div>
+    </div>
+    </div>
+  );
+}
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,13 @@
 import Content from '../../component/home/content';
+import Image from '../../component/home/image';
+import Success from '../../component/home/success';
 
 export default function Home() {
     return (
+        <div>
         <Content/>
+        <Image/>
+        <Success/>
+        </div>
     );
     }
